78d739701dec2dd3f96eeac2da8b5f48d4632251
aaronalberg <aalberg2@illinois.edu>
2020-01-30 11:59:49 -0600

    boston API added

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -7,7 +7,7 @@ class Nav extends React.Component {
     
     componentDidMount(){
         
-    fetch('http://lapi.transitchicago.com/api/1.0/ttpositions.aspx?key=0e726cb9cf35433f9dcdf4d34504c50f&rt=red')
+    fetch('https://api-v3.mbta.com/predictions?filter[stop]=place-sstat&filter[direction_id]=0&include=stop')
 .then(function(response) {
   console.log(response.json());
 })
