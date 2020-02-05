dd648fd90a33c28374e3e4fddadb4b9a4aa1587b
aaronalberg <aalberg2@illinois.edu>
2020-02-05 12:27:17 -0600

    backend starting

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './Components/Nav';
+import API from './Components/API';
 
 function App() {
 
-    
-    
-    
-  return (
+    return (
     <div className="App">
       <Nav />
+      <API />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,6 +19,10 @@ function App() {
       </header>
     </div>
   );
+        
+
+
 }
+
 
 export default App;
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,47 +4,21 @@ import '../App.css';
 class Nav extends React.Component {
     
     
-    
+    /*
     componentDidMount(){
-      /*  
-    fetch('https://api-v3.mbta.com/predictions?filter[stop]=place-sstat&filter[direction_id]=0&include=stop')
-.then(function(response) {
-  console.log(response.json());
-})
-.catch(function(error) {
-  console.log('Looks like there was a problem: \n', error);
-});
-        
-        console.log('heheheh');
-        fetch('https://crossorigin.me/http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=0e726cb9cf35433f9dcdf4d34504c50f&max=1&mapid=40360')
-.then(function(response) {
-  console.log(response.json());
-    console.log('heheheh');
-})
-.catch(function(error) {
-  console.log('Looks like there was a problem: \n', error);
-});
-      */
-        fetch('https://cors-anywhere.herokuapp.com/http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=0e726cb9cf35433f9dcdf4d34504c50f&max=1&mapid=40360', {mode: 'cors'})
-  .then(function(response) {
-    return response.text();
-  })
-  .then(function(text) {
-    console.log('Request successful', text);
-  })
-  .catch(function(error) {
-    console.log('Request failed', error)
-  });
-
+    
     }
+    */
+
+    
+
 
   render() {
-    return( 
-        <><h1>Nearby Stops {this.props.name}</h1>
-      <h1>Favorite Stops</h1>
-      <h1>Header</h1>
-        </>
-  );}
+    return ( 
+      <div>
+      </div>
+    );
+}
 }
 
 export default Nav;
